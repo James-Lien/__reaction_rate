e07b90c98b5da64af87222f7fe2fd190905159b1
連照銘 <cm.lien.go@gmail.com>
2026-05-15 16:17:34 +0800

    修改四

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/__reaction_rate/',
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 });
